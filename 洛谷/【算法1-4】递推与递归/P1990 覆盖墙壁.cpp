bffcf068f39e616e6f74a49448712fd13a696101
a1050562365 <1050562365@qq.com>
2021-02-07 17:32:22 +0800

    2021-02-07

---
+++ 洛谷/【算法1-4】递推与递归/P1990 覆盖墙壁.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+    int n;
+    int dp[2] = {1, 2};
+    int s = 1;
+    scanf("%d", &n);
+    for (int i = 2; i < n; i++)
+    {
+        int newdp = (s * 2 + dp[0] + dp[1]) % 10000;
+        s += dp[0];
+        s = s % 10000; 
+        dp[0] = dp[1];
+        dp[1] = newdp;
+    }
+    printf("%d", dp[n >= 3 ? 2 : n]);
+}
